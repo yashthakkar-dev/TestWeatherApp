5a917da8d5e09ba3f9644b9a57b69050f928ffb4
Yash Thakkar <ythakkar@thoughttechnology.com>
2025-07-18 04:41:26 -0400

    update location implementation

--- app/src/main/java/com/example/testweatherapp/data/location/LocationProvider.kt
+++ app/src/main/java/com/example/testweatherapp/data/location/LocationProvider.kt
@@ -1,13 +1,10 @@
 package com.example.testweatherapp.data.location
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Looper
 import android.util.Log
-import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationAvailability
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -26,44 +23,51 @@ class LocationProvider @Inject constructor(
     companion object {
         val TAG: String = LocationProvider::class.java.simpleName
     }
+
     private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
     @SuppressLint("MissingPermission")
     suspend fun getCurrentLocation(): Location? = withTimeoutOrNull(10000L) {
 
-        val lastLocation = fusedLocationClient.lastLocation.await() // Use kotlinx-coroutines-play-services
-        if (lastLocation != null) {
-            Log.d(TAG, "Returning last known location: $lastLocation")
-            return@withTimeoutOrNull lastLocation
-        }
+        val lastLocation = fusedLocationClient.lastLocation.await()
+        Log.d(TAG, "Last known location (fallback): $lastLocation")
 
-        // If not available, proceed to request active location
-        suspendCancellableCoroutine { cont ->
+        // proceed to request active location
+        suspendCancellableCoroutine { continuation ->
             val locationRequest = LocationRequest.Builder(
                 Priority.PRIORITY_HIGH_ACCURACY, 1000
-            ).apply {
-                setWaitForAccurateLocation(false)
-                setMaxUpdates(5)
-            }.build()
+            ).setWaitForAccurateLocation(false)
+                .setMaxUpdates(5)
+                .build()
 
             val locationCallback = object : LocationCallback() {
                 override fun onLocationResult(result: LocationResult) {
-                    if (cont.isActive && result.locations.isNotEmpty()) {
+                    if (continuation.isActive && result.locations.isNotEmpty()) {
                         Log.d(TAG, "Location found: ${result.lastLocation}")
                         fusedLocationClient.removeLocationUpdates(this)
-//                        cont.resume(result.lastLocation)
+                        result.lastLocation?.let {
+                            continuation.resume(it)
+                        } ?: run {
+                            Log.d(TAG, "Falling back to last known location")
+                            if (lastLocation != null) {
+                                continuation.resume(lastLocation)
+                            } else {
+                                continuation.resumeWith(Result.failure(NullPointerException("Location is null")))
+                            }
+                        }
                     }
                 }
+
                 override fun onLocationAvailability(availability: LocationAvailability) {
-                    if (cont.isActive && !availability.isLocationAvailable) {
+                    if (continuation.isActive && !availability.isLocationAvailable) {
                         Log.d(TAG, "Location not available")
                         fusedLocationClient.removeLocationUpdates(this)
-//                        cont.resume(null)
+                        continuation.resumeWith(Result.failure(NullPointerException("Location unavailable")))
                     }
                 }
             }
 
-            cont.invokeOnCancellation {
+            continuation.invokeOnCancellation {
                 fusedLocationClient.removeLocationUpdates(locationCallback)
             }
 
